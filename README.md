5a0fab262b657a4126af2badfc17ce40f78a0704
Mario Geiger <geiger.mario@gmail.com>
2023-06-15 10:25:15 -0400

    add kuhn_munkres

--- README.md
+++ README.md
@@ -2,19 +2,21 @@
 
 Binding of [pathfinding](https://github.com/samueltardieu/pathfinding) for Python.
 
+## Dependencies
+
+- [Rust](https://www.rust-lang.org/tools/install)
+- [numpy](https://numpy.org/) (`pip install numpy`)
+
 ## Installation
 
 ```bash
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 pip install .
 ```
 
 ## Progress
 
-- [x] `pathfinding::kuhn_munkres::kuhn_munkres_min`
-- [ ] `pathfinding::kuhn_munkres::kuhn_munkres`
-- [ ] `pathfinding::directed::*`
-- [ ] all the rest...
+- `pathfinding::kuhn_munkres::kuhn_munkres`
+- `pathfinding::kuhn_munkres::kuhn_munkres_min`
 
 ## Testing
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use ::pathfinding::prelude::{kuhn_munkres_min, Matrix};
-use numpy::ndarray::{Array1, Array2};
+use ::pathfinding::prelude::{kuhn_munkres, kuhn_munkres_min, Matrix};
+use numpy::ndarray::{Array1, ArrayView2};
 use numpy::{IntoPyArray, PyArray1, PyReadonlyArray2};
 use ordered_float::NotNan;
 use pyo3::{pymodule, types::PyModule, PyResult, Python};
@@ -7,15 +7,32 @@ use pyo3::{pymodule, types::PyModule, PyResult, Python};
 #[pymodule]
 fn pathfinding(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     #[pyfn(m)]
+    #[pyo3(name = "kuhn_munkres")]
+    fn kuhn_munkres_py<'py>(
+        py: Python<'py>,
+        weights: PyReadonlyArray2<'_, f64>,
+    ) -> PyResult<(f64, &'py PyArray1<i32>)> {
+        let weights: ArrayView2<f64> = weights.as_array();
+        let weights = Matrix::from_rows(weights.rows()).unwrap();
+        let weights = weights.map(|x| NotNan::new(*x).unwrap());
+
+        let (cost, assignments) = kuhn_munkres(&weights);
+
+        let cost: f64 = cost.into_inner();
+        let assignments: Vec<i32> = assignments.iter().map(|x| *x as i32).collect();
+        Ok((cost, Array1::from(assignments).into_pyarray(py)))
+    }
+
+    #[pyfn(m)]
     #[pyo3(name = "kuhn_munkres_min")]
     fn kuhn_munkres_min_py<'py>(
         py: Python<'py>,
         weights: PyReadonlyArray2<'_, f64>,
     ) -> PyResult<(f64, &'py PyArray1<i32>)> {
-        let weights = weights.as_array();
-        let weights: Array2<NotNan<f64>> = weights.map(|x| NotNan::new(*x).unwrap());
+        let weights: ArrayView2<f64> = weights.as_array();
         let weights = Matrix::from_rows(weights.rows()).unwrap();
-        let weights = weights.map(|x| *x);
+        let weights = weights.map(|x| NotNan::new(*x).unwrap());
+
         let (cost, assignments) = kuhn_munkres_min(&weights);
 
         let cost: f64 = cost.into_inner();
